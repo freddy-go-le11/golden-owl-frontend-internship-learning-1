89c15ebeedd7700fe6d29e6a1861ccfc47995f45
freddy <freddy.le.goldenowl@gmail.com>
2024-11-25 11:46:27 +0700

    refactor: improve readability and consistency in useRetry hook

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,2 +1,7 @@
 export const LOCALES = ["en", "vi"];
 export const DEFAULT_LOCALE = "vi";
+
+export const SECOND_IN_MILLISECONDS = 1000;
+export const MINUTE_IN_MILLISECONDS = SECOND_IN_MILLISECONDS * 60;
+export const HOUR_IN_MILLISECONDS = MINUTE_IN_MILLISECONDS * 60;
+export const DAY_IN_MILLISECONDS = HOUR_IN_MILLISECONDS * 24;
--- src/hooks/use-retry.tsx
+++ src/hooks/use-retry.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 
-import { useCookies } from 'react-cookie';
+import { SECOND_IN_MILLISECONDS } from "@/common/constants";
+import { useCookies } from "react-cookie";
 
 interface RetryOptions {
   maxFailedAttempts?: number; // Max failed attempts before blocking
@@ -19,35 +20,42 @@ interface UseRetryReturn {
 const useRetry = ({
   maxFailedAttempts = 3,
   blockTime = 60,
-  userKey
+  userKey,
 }: RetryOptions): UseRetryReturn => {
-  const [cookies, setCookie, removeCookie] = useCookies([`blockTimeRemaining-${userKey}`, `isBlocked-${userKey}`]);
+  const [cookies, setCookie, removeCookie] = useCookies([
+    `blockTimeRemaining-${userKey}`,
+    `isBlocked-${userKey}`,
+  ]);
   const [failedAttempts, setFailedAttempts] = useState(0);
 
-  const getCurrentTime = () => Math.floor(Date.now() / 1000);
+  const getCurrentTime = useCallback(() => Math.floor(Date.now() / 1000), []);
 
   // Get the remaining block time and blocked status from cookies
   const blockTimeRemainingFromCookie = cookies[`blockTimeRemaining-${userKey}`]
     ? parseInt(cookies[`blockTimeRemaining-${userKey}`])
     : 0;
-  const isBlockedFromCookie = cookies[`isBlocked-${userKey}`] === 'true';
+  const isBlockedFromCookie = cookies[`isBlocked-${userKey}`] === true;
 
   const [isBlocked, setIsBlocked] = useState(isBlockedFromCookie);
-  const [blockTimeRemaining, setBlockTimeRemaining] = useState(blockTimeRemainingFromCookie);
+  const [blockTimeRemaining, setBlockTimeRemaining] = useState(
+    blockTimeRemainingFromCookie
+  );
 
   const startBlock = useCallback(() => {
     setIsBlocked(true);
     const releaseTime = getCurrentTime() + blockTime; // Block until current time + blockTime
     setBlockTimeRemaining(blockTime);
-    setCookie(`isBlocked-${userKey}`, 'true', { path: '/' });
-    setCookie(`blockTimeRemaining-${userKey}`, releaseTime.toString(), { path: '/' });
-  }, [blockTime, setCookie, userKey]);
+    setCookie(`isBlocked-${userKey}`, "true", { path: "/" });
+    setCookie(`blockTimeRemaining-${userKey}`, releaseTime.toString(), {
+      path: "/",
+    });
+  }, [blockTime, getCurrentTime, setCookie, userKey]);
 
   const resetBlock = useCallback(() => {
     setIsBlocked(false);
     setFailedAttempts(0);
-    removeCookie(`isBlocked-${userKey}`, { path: '/' });
-    removeCookie(`blockTimeRemaining-${userKey}`, { path: '/' });
+    removeCookie(`isBlocked-${userKey}`, { path: "/" });
+    removeCookie(`blockTimeRemaining-${userKey}`, { path: "/" });
   }, [removeCookie, userKey]);
 
   const handleFailedAttempt = useCallback(() => {
@@ -61,21 +69,33 @@ const useRetry = ({
   useEffect(() => {
     // If already blocked, check the remaining time for the block
     if (isBlocked) {
-      const currentTime = getCurrentTime();
-      const releaseTime = parseInt(cookies[`blockTimeRemaining-${userKey}`] || '0');
-      if (releaseTime <= currentTime) {
-        resetBlock(); // Unblock if the block time has passed
-      } else {
-        const remainingTime = releaseTime - currentTime;
-        setBlockTimeRemaining(remainingTime);
-      }
+      const intervalId = setInterval(() => {
+        const currentTime = getCurrentTime();
+        const releaseTime = parseInt(
+          cookies[`blockTimeRemaining-${userKey}`] || "0"
+        );
+
+        if (releaseTime <= currentTime) {
+          resetBlock(); // Unblock if the block time has passed
+          clearInterval(intervalId); // Stop checking after unblock
+        } else {
+          const remainingTime = releaseTime - currentTime;
+          setBlockTimeRemaining(remainingTime); // Update remaining block time
+        }
+      }, SECOND_IN_MILLISECONDS); // Run every 1 second
+
+      // Cleanup the interval when the component unmounts or isBlocked changes
+      return () => clearInterval(intervalId);
     }
-  }, [isBlocked, cookies, resetBlock, userKey]);
+  }, [isBlocked, cookies, resetBlock, userKey, getCurrentTime]);
 
   useEffect(() => {
     // If the page is refreshed or revisited, check if the user is still blocked
     const currentTime = getCurrentTime();
-    if (cookies[`isBlocked-${userKey}`] === 'true' && cookies[`blockTimeRemaining-${userKey}`]) {
+    if (
+      cookies[`isBlocked-${userKey}`] === true &&
+      cookies[`blockTimeRemaining-${userKey}`]
+    ) {
       const releaseTime = parseInt(cookies[`blockTimeRemaining-${userKey}`]);
       if (releaseTime > currentTime) {
         setIsBlocked(true);
@@ -84,14 +104,14 @@ const useRetry = ({
         resetBlock(); // Unblock if the time has passed
       }
     }
-  }, [cookies, resetBlock, userKey]);
+  }, [cookies, getCurrentTime, resetBlock, userKey]);
 
   return {
     isBlocked,
     blockTimeRemaining,
     failedAttempts,
     resetBlock,
-    handleFailedAttempt
+    handleFailedAttempt,
   };
 };
 
