335ceaf858c58a6bb7edfddff5a60acf41fc84b8
freddy <freddy.le.goldenowl@gmail.com>
2024-11-28 17:02:47 +0700

    fix: update parameter type in RegisterPage component for improved type safety

--- src/app/[locale]/(auth)/register/page.tsx
+++ src/app/[locale]/(auth)/register/page.tsx
@@ -6,7 +6,7 @@ export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
 
-export default async function RegisterPage({ params }: IDefaultLayoutProps) {
+export default async function RegisterPage({ params }: IDefaultPageProps) {
   const { locale } = await params;
 
   setRequestLocale(locale);
